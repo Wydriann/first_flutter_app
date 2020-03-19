0f45e954736bda1142cfbc5dd16acff19867204a
Wydrian Matheus <wydrian.alexandre@gmail.com>
2020-03-19 00:31:59 -0300

    Botoes e slide de imagens

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,41 +5,96 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Olá flutter",
-        ),
-        centerTitle: true,
+        title: Text("Hello Flutter"),
       ),
-      body: body(),
+      body: _body(),
     );
   }
 
-  body() {
-   return Container(
-      margin: EdgeInsets.only(left: 10, top: 10),
-        color: Colors.white,
-        child: _ing(),
-      );
+  _body() {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _text(),
+          Container(
+            height: 300,
+            child: PageView(
+              children: <Widget>[
+                _img(),
+                _img(),
+                _img(),
+                _img(),
+                _img(),
+              ],
+            ),
+          ),
+          _buttons(),
+        ],
+      ),
+    );
   }
-  _ing() {
+
+  _buttons() {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _button("ListView"),
+            _button("Page 2"),
+            _button("Page 3")
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _button("Snack"),
+            _button("Dialog"),
+            _button("Toast")
+          ],
+        )
+      ],
+    );
+  }
+
+  _button(String text) {
+    return RaisedButton(
+        color: Colors.blue,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+        onPressed: _onClickOk
+    );
+  }
+
+  void _onClickOk() {
+    print("Clicou no botão!");
+  }
+
+  _img() {
     return Image.asset(
-        "assets/images/dog5.png",
-         );
+      "assets/images/dog1.png",
+      fit: BoxFit.cover,
+    );
   }
+
   _text() {
     return Text(
-      "Olá mundo",
+      "Hello World",
       style: TextStyle(
-        fontSize: 30,
-        color: Colors.blue,
-        fontWeight: FontWeight.bold,
-        fontStyle: FontStyle.italic,
-        decoration: TextDecoration.underline,
-        decorationColor: Colors.red,
-        decorationStyle: TextDecorationStyle.wavy,
-      ),
+          color: Colors.blue,
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          fontStyle: FontStyle.italic,
+          decoration: TextDecoration.underline,
+          decorationColor: Colors.red,
+          decorationStyle: TextDecorationStyle.wavy),
     );
   }
-
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primaryColor: Colors.red),
+      theme: ThemeData(primaryColor: Colors.yellow),
       home: HomePage(),
     );
   }
